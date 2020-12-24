542074cd8e68251546eda489aab63c32a3fefa25
prachi12375 <75881303+prachi12375@users.noreply.github.com>
2020-12-24 17:44:55 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# bulletsandwall
-This project is to test the reliability of walls when bullets hit them. They test the effectiveness of walls against bullets, among other things. All walls are made of concrete material. If the damage to the wall from the bullet exceeds a certain value, the material used to make the wall is rejected.
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,44 @@
+var bullet, wall, thickness;
+var speed, weight;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed = random(223, 321);
+  weight = random(30, 52);
+  thickness = random(22, 83);
+
+  bullet = createSprite(50, 200, 50, 10);
+  bullet.shapeColor="white";
+  bullet.velocityX = speed;
+
+  wall = createSprite(1200, 200, thickness, height/2);
+  wall.shapeColor=rgb(80, 80, 80);
+}
+
+function draw() {
+  background(0, 0, 0);
+  
+if(hasCollided(bullet, wall)){
+  bullet.velocityX=0;
+  var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+  if(damage>10){
+    wall.shapeColor=rgb(225, 0, 0);
+  }
+
+  if(damage<10){
+    wall.shapeColor=rgb(0, 255, 0);
+  }
+}
+  drawSprites();
+}
+function hasCollided(lbullet, lwall){
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  return false;
+}
